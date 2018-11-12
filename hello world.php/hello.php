3cdab42088d2d17cf66f121ba29724a7db9c4def
Melomanu <44948251+Melomanu@users.noreply.github.com>
2018-11-12 19:29:38 +0200

    Add files via upload
    Din clasa

---
+++ hello world.php/hello.php
@@ -0,0 +1,5 @@
+<?php
+echo 'Introduceti numele \n';
+$nume = readline()
+  echo "Heloo, $nume". PHP_EOL;
+?>
---
+++ hello world.php/variabila.php
@@ -0,0 +1,14 @@
+<?php
+  $firstname = 'Ion';
+  $firstName = 'Vasile';
+  echo "$firstname, $firstName, $FirstName".PHP_EOL;
+
+    define(MYCONSTANT, 'Something');
+    echo MYCONSTANT.PHP_EOL;
+      echo 'Versiunea de PHP:'. PHP_VERSION.PHP_EOL;
+      echo 'Executabilul PHP folosit:'.PHP_BINARY.PHP_EOL;
+      echo 'Sistemul de operare:'.PHP_OS.PHP_EOL;
+      echo 'Linia curenta:'.__LINE__.PHP_EOL;
+      echo 'Fisierul curent:'.__FILE__.PHP_EOL;
+      echo 'Directorul curent:'.__DIR__.PHP_EOL;
+?>
